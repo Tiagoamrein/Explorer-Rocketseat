c92fd07a8da1ba16c454c2faa7c83e59cffb128b
tiago <tiagoamrein@gmail.com>
2022-08-16 23:17:57 -0300

    update

---
+++ Explorer-Rocketseat/Desafios/FocusTimer 2.0/js/sounds.js
@@ -0,0 +1,18 @@
+export default function() { 
+  const forestAudio = new Audio("./Audio/Floresta.wav")
+  const rainAudio = new Audio("./Audio/Chuva.wav")
+  const coffeShopAudio = new Audio("./Audio/Cafeteria.wav")
+  const fireAudio = new Audio("./Audio/Lareira.wav")
+
+  forestAudio.loop = true
+  rainAudio.loop = true
+  coffeShopAudio.loop = true
+  fireAudio.loop = true
+
+  return {
+    forestAudio,
+    rainAudio,
+    coffeShopAudio,
+    fireAudio
+  }
+}
---
+++ Explorer-Rocketseat/Desafios/FocusTimer Dark/js/elements.js
@@ -0,0 +1,36 @@
+const buttonPlay = document.querySelector('.play')
+const buttonStop = document.querySelector('.stop')
+const buttonAdd = document.querySelector('.add')
+const buttonRemove = document.querySelector('.remove')
+const buttonForest = document.querySelector('.forest')
+const buttonRain = document.querySelector('.rain')
+const buttonCoffeShop = document.querySelector('.coffe-shop')
+const buttonFire = document.querySelector('.fire')
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+const buttonLightMode = document.querySelector('.light-mode')
+const buttonDarkMode = document.querySelector('.dark-mode')
+const volumeForest = document.querySelector('.volume-forest')
+const volumeRain = document.querySelector('.volume-rain')
+const volumeCoffe = document.querySelector('.volume-coffe')
+const volumeFire = document.querySelector('.volume-fire')
+
+
+export {
+  buttonPlay,
+  buttonStop,
+  buttonAdd,
+  buttonRemove,
+  buttonForest,
+  buttonRain,
+  buttonCoffeShop,
+  buttonFire,
+  minutesDisplay,
+  secondsDisplay,
+  buttonLightMode,
+  buttonDarkMode,
+  volumeForest,
+  volumeRain,
+  volumeCoffe,
+  volumeFire,
+}
