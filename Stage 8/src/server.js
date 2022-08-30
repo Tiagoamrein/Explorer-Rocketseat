2e8f77bb684c5a25846215a419f99387edd8b9bd
tiago <tiagoamrein@gmail.com>
2022-08-29 21:10:37 -0300

    att repo

--- Stage 8/src/server.js
+++ Stage 8/src/server.js
@@ -1,6 +1,13 @@
+const { request, response } = require('express');
 const express = require ('express')
 
 const app = express();
+app.use (express.json())
+
+app.post ("/users", (request, response) => {
+  const {name, email, password} = request.body
+  response.json({name, email, password})
+})
 
 const PORT = 3333;
 app.listen(PORT, () => console.log (`server is running on port ${PORT}`) );
