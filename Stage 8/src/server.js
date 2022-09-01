db2b9d72877b7fe44c568fb7e7a4029a1fcbb4b2
tiago <tiagoamrein@gmail.com>
2022-09-01 20:34:39 -0300

    add controllers

--- Stage 8/src/server.js
+++ Stage 8/src/server.js
@@ -1,13 +1,12 @@
 const { request, response } = require('express');
+
 const express = require ('express')
+
+const routes = require("./routes")
 
 const app = express();
 app.use (express.json())
 
-app.post ("/users", (request, response) => {
-  const {name, email, password} = request.body
-  response.json({name, email, password})
-})
-
+app.use (routes)
 const PORT = 3333;
 app.listen(PORT, () => console.log (`server is running on port ${PORT}`) );
