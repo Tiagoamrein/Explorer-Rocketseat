c4a8481a80d872e396f52a6c455122a3b226af9b
tiago <tiagoamrein@gmail.com>
2022-09-05 22:22:23 -0300

    update stage 8

--- Stage 8/src/server.js
+++ Stage 8/src/server.js
@@ -1,5 +1,7 @@
-const { request, response } = require('express');
 
+require ("express-async-errors")
+const AppError = require ("./utils/AppError")
+const { request, response } = require("express");
 const express = require ('express')
 
 const routes = require("./routes")
@@ -8,5 +10,20 @@ const app = express();
 app.use (express.json())
 
 app.use (routes)
+app.use ((error, request, response, next)=> {
+  if(error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message : error.message
+    })
+  }
+
+  console.log (error)
+
+  return response.status (500).json({
+    status: "error",
+    message: "Internal server error"
+  })
+})
 const PORT = 3333;
 app.listen(PORT, () => console.log (`server is running on port ${PORT}`) );
