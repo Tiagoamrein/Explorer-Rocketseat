01db9964664d257eebcf683dcf3f7d4f6f74ebf5
tiago <tiagoamrein@gmail.com>
2022-09-24 14:26:01 -0300

    Att stage 8

--- Stage 8/src/routes/users.routes.js
+++ Stage 8/src/routes/users.routes.js
@@ -1,11 +1,19 @@
-const  {Router} = require("express")
+const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload")
 
-const UsersController = require ("../controllers/UsersController")
-const usersRoutes = Router()
+const UserController = require("../controllers/UserController")
+const UserAvatarController = require("../controllers/UserAvatarController")
+const ensureAuthenticated = require("../middleware/ensureAuthenticated")
 
-const usersController = new UsersController() 
+const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER)
 
+const userController = new UserController();
+const userAvatarController = new UserAvatarController()
 
-usersRoutes.post("/", usersController.create)
+usersRoutes.post("/", userController.create)
+usersRoutes.put("/", ensureAuthenticated, userController.update)
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update)
 
 module.exports = usersRoutes;
